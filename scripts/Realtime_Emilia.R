1bb9ecd6f78f5bcc01db8d8e4486a12d908db3bc
Floor Buschenhenke <floor.buschenhenke@huygens.knaw.nl>
2022-10-12 13:44:55 +0200

    Added startdates and times for couple more writing sessions

--- scripts/Realtime_Emilia.R
+++ scripts/Realtime_Emilia.R
@@ -64,7 +64,7 @@ write.csv(matrix7, "data/emilia_revmatrix_sessionid_1.csv", row.names = FALSE)
 ### session id 2 ####
 
 # first set start and endtimes to 0 at the start of the session
-matrix1 <- read.csv("data/revisionmatrix.csv")
+matrix1 <- read.csv("data/revisionmatrix_Emilia.csv")
 names(matrix1)
 
 matrix2 <- matrix1 %>%
@@ -118,70 +118,12 @@ write.csv(matrix7, "data/emilia_revmatrix_sessionid_2.csv", row.names = FALSE)
 
 
 
-### vanaf hier nog aanpassen naar emilia! ####
-
-### session id 3 ####
-
-# first set start and endtimes to 0 at the start of the session
-matrix1 <- read.csv("data/revisionmatrix.csv")
-names(matrix1)
-
-matrix2 <- matrix1 %>%
-  filter(session_number == "2")%>%
-  mutate(starttijdschoon = start_time - first(start_time),
-         eindtijdschoon = (end_time - start_time) + starttijdschoon)
-
-#view(matrix2)
 
 
-
-# add real session start clock time and date 
-# change for each session! 
-matrix3 <- matrix2 %>%
-  mutate(startdatumtijd = ymd_hms("2020-12-08 11:21:56"))
-
-#view(matrix3)
-
-# convert event start and endtimes to datetime format 
-
-matrix4 <- matrix3 %>%
-  mutate(starttijdklok1 = seconds_to_period(starttijdschoon/1000))
-
-#view(matrix4)
-
-matrix5 <- matrix4 %>%
-  mutate(starttijdklok2 = startdatumtijd + starttijdklok1)
-
-#view(matrix5)
-
-### format aanpassen (platte string van maken om seq-element in TEI te matchen)
-
-matrix6 <- matrix5 %>%
-  mutate(seq_element_starttime1 = as.character(starttijdklok2))
-
-matrix6$seq_element_starttime2 <- gsub('[-: ]', '', matrix6$seq_element_starttime1)
-
-#view(matrix6)
-names(matrix6)
-#clean, remove helper columns
-matrix7 <- matrix6 %>%
-  select(-startdatumtijd, -starttijdklok1, -starttijdklok2, -seq_element_starttime1)%>%
-  rename (seq_element = seq_element_starttime2)%>%
-  relocate(seq_element, .before = start_id_GA)%>%
-  relocate(start_position, .before = start_time_rel)%>%
-  relocate(end_position, .after = start_position)
-
-#view(matrix7)
-
-write.csv(matrix7, "data/rova_revmatrix_sessionid_2.csv", row.names = FALSE)
-
-
-
-
-### session id 3 ####
+### session id 3 emilia ####
 
 # first set start and endtimes to 0 at the start of the session
-matrix1 <- read.csv("data/revisionmatrix.csv")
+matrix1 <- read.csv("data/revisionmatrix_Emilia.csv")
 names(matrix1)
 
 matrix2 <- matrix1 %>%
@@ -195,8 +137,9 @@ matrix2 <- matrix1 %>%
 
 # add real session start clock time and date 
 # change for each session! 
+
 matrix3 <- matrix2 %>%
-  mutate(startdatumtijd = ymd_hms("2020-12-08 14:05:49"))
+  mutate(startdatumtijd = ymd_hms("2020-11-25 17:17:12"))
 
 #view(matrix3)
 
@@ -231,12 +174,15 @@ matrix7 <- matrix6 %>%
 
 #view(matrix7)
 
-write.csv(matrix7, "data/rova_revmatrix_sessionid_3.csv", row.names = FALSE)
+write.csv(matrix7, "data/emilia_revmatrix_sessionid_3.csv", row.names = FALSE)
+
+
+
 
 ### session id 4 ####
 
 # first set start and endtimes to 0 at the start of the session
-matrix1 <- read.csv("data/revisionmatrix.csv")
+matrix1 <- read.csv("data/revisionmatrix_Emilia.csv")
 names(matrix1)
 
 matrix2 <- matrix1 %>%
@@ -250,8 +196,9 @@ matrix2 <- matrix1 %>%
 
 # add real session start clock time and date 
 # change for each session! 
+
 matrix3 <- matrix2 %>%
-  mutate(startdatumtijd = ymd_hms("2020-12-09 12:29:38"))
+  mutate(startdatumtijd = ymd_hms("2020-11-30 16:30:29"))
 
 #view(matrix3)
 
@@ -286,15 +233,18 @@ matrix7 <- matrix6 %>%
 
 #view(matrix7)
 
-write.csv(matrix7, "data/rova_revmatrix_sessionid_4.csv", row.names = FALSE)
+write.csv(matrix7, "data/emilia_revmatrix_sessionid_4.csv", row.names = FALSE)
+
+
 
 
 
 ### session id 5 ####
 
 # first set start and endtimes to 0 at the start of the session
-matrix1 <- read.csv("data/revisionmatrix.csv")
+matrix1 <- read.csv("data/revisionmatrix_Emilia.csv")
 names(matrix1)
+
 
 matrix2 <- matrix1 %>%
   filter(session_number == "5")%>%
@@ -305,10 +255,11 @@ matrix2 <- matrix1 %>%
 
 
 
+
 # add real session start clock time and date 
 # change for each session! 
 matrix3 <- matrix2 %>%
-  mutate(startdatumtijd = ymd_hms("2020-12-09 12:56:40"))
+  mutate(startdatumtijd = ymd_hms("2020-12-08 11:48:04"))
 
 #view(matrix3)
 
@@ -343,9 +294,65 @@ matrix7 <- matrix6 %>%
 
 #view(matrix7)
 
-write.csv(matrix7, "data/rova_revmatrix_sessionid_5.csv", row.names = FALSE)
+write.csv(matrix7, "data/emilia_revmatrix_sessionid_5.csv", row.names = FALSE)
 
 
+### session id 6 ####
+
+# first set start and endtimes to 0 at the start of the session
+matrix1 <- read.csv("data/revisionmatrix_Emilia.csv")
+names(matrix1)
+
+matrix2 <- matrix1 %>%
+  filter(session_number == "6")%>%
+  mutate(starttijdschoon = start_time - first(start_time),
+         eindtijdschoon = (end_time - start_time) + starttijdschoon)
+
+#view(matrix2)
+
+
+# add real session start clock time and date 
+# change for each session! 
+matrix3 <- matrix2 %>%
+  mutate(startdatumtijd = ymd_hms("2020-12-08 13:44:24"))
+
+#view(matrix3)
+
+# convert event start and endtimes to datetime format 
+
+matrix4 <- matrix3 %>%
+  mutate(starttijdklok1 = seconds_to_period(starttijdschoon/1000))
+
+#view(matrix4)
+
+matrix5 <- matrix4 %>%
+  mutate(starttijdklok2 = startdatumtijd + starttijdklok1)
+
+#view(matrix5)
+
+### format aanpassen (platte string van maken om seq-element in TEI te matchen)
+
+matrix6 <- matrix5 %>%
+  mutate(seq_element_starttime1 = as.character(starttijdklok2))
+
+matrix6$seq_element_starttime2 <- gsub('[-: ]', '', matrix6$seq_element_starttime1)
+
+#view(matrix6)
+names(matrix6)
+#clean, remove helper columns
+matrix7 <- matrix6 %>%
+  select(-startdatumtijd, -starttijdklok1, -starttijdklok2, -seq_element_starttime1)%>%
+  rename (seq_element = seq_element_starttime2)%>%
+  relocate(seq_element, .before = start_id_GA)%>%
+  relocate(start_position, .before = start_time_rel)%>%
+  relocate(end_position, .after = start_position)
+
+#view(matrix7)
+
+write.csv(matrix7, "data/emilia_revmatrix_sessionid_6.csv", row.names = FALSE)
+
+
+### vanaf hier nog aanpassen naar emilia! ####
 ### session id 6 ####
 
 # first set start and endtimes to 0 at the start of the session
